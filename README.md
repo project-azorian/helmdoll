00ae4081e8b7ba88aeeda59d9ad4f5c8bde996ec
Pete Birley <petebirley+github@gmail.com>
2022-08-24 09:55:03 -0500

    Create README.md
    Signed-off-by: Pete Birley <petebirley@microsoft.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# helmdoll
+
+This is a demo of two things:
+ * using one chart to manage another
+ * the ability to update CRDs deployed via a helm chart
--- demo.sh
+++ demo.sh
@@ -3,10 +3,12 @@ set -x
 
 # build the 'fragile' chart and package it within the 'robust' wrapper chart.
 helm package fragile
+rm -fv ./robust/files/fragile-0.1.0.tgz
 mv -v fragile-0.1.0.tgz ./robust/files/
 
 # install the 'robust' wrapper chart, which in turn deploys the 'fragile' chart
-helm upgrade --install robust ./robust
+helm upgrade --install --wait robust ./robust
+
 helm status fragile
 
 # remove the 'robust' release, and validate that the 'fragile' release still exists
@@ -14,5 +16,5 @@ helm delete robust
 helm status fragile
 
 # re-install the 'robust' release, and note that the fragile release revision has incremented again
-helm upgrade --install robust ./robust
+helm upgrade --install --wait robust ./robust
 helm status fragile
